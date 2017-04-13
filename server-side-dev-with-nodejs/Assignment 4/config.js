0d1ac9121c7dec05255aff41db14d0d6516734cd
Hesham <heshamusama@gmail.com>
2017-04-13 19:40:02 +0200

    [server-side-dev-with-nodejs] Assignment 4

---
+++ server-side-dev-with-nodejs/Assignment 4/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '434364606915740',
+        clientSecret: 'e772a0fd5155b3497ef782b873cf766a',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
+}
